46c11818dd5cfdd5124338083b77dbb6a9cfa3f8
guimoreiraflauzino <guimoreirapimentel@gmail.com>
2025-03-08 10:08:16 -0300

    first commit

---
+++ Aula_01/Exemplo.js
@@ -0,0 +1,34 @@
+console.log("olá ");
+
+var animal = "Alefante";
+
+console.log(animal);
+console.log(typeof animal);
+
+
+animal = "cachorro ";
+console.log(animal);
+
+const valor = 10;
+
+//valor = 10;
+
+
+
+var nomeCompleto = window.prompt("Nome:");
+var idade = parseInt(window.prompt("Idade:"));
+
+console.log(nomeCompleto);
+console.log( idade);
+console.log(typeof idade);
+
+console.log(`Vo se chama: ${nomeCompleto} e tem ${idade} anos`);
+
+console.log(5 == "5");//comparação
+
+console.log(5 === "5");//estritamente igual
+
+console.log(5 !== "5");
+
+
+
